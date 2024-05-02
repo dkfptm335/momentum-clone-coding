6ef0e938cfc5f1be9ea128b80a87b3034a205ba8
Gloomy <dkfptm335@naver.com>
2024-05-02 23:41:03 +0900

    javascript study

---
+++ js/greetings.js
@@ -0,0 +1,38 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("h1");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
+}
+
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// function handleLinkClick(event) {
+//     event.preventDefault();
+//     console.dir(event);
+// }
+
+loginForm.addEventListener("submit", onLoginSubmit);
+// link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the h1
+    paintGreetings();
+}
